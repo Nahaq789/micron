442d4f338eaa29e9e85369ea230aa357cfc0df66
Nahaq789 <naha022677@icloud.com>
2025-06-11 08:15:18 +0900

    logger init

--- services/user_service/cmd/api/main.go
+++ services/user_service/cmd/api/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"context"
 	"log/slog"
+	"os"
+	"user_service/internal/logger"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	ctx := context.Background()
+
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	contextHandler := logger.NewContextHandler(handler)
+	logger := slog.New(contextHandler)
+	slog.SetDefault(logger)
+
 	r := gin.Default()
 	if err := Router(ctx, r); err != nil {
 		slog.Error("failed to start router", "error", err)

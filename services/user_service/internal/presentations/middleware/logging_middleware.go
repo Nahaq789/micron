1c38ec97cb19d0f69db5120c89f34e0f2baf010c
Nahaq789 <naha022677@icloud.com>
2025-06-16 19:03:27 +0900

    fix: logging middleware

--- services/user_service/internal/presentations/middleware/logging_middleware.go
+++ services/user_service/internal/presentations/middleware/logging_middleware.go
@@ -1,9 +1,46 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"log/slog"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type requestInfo struct {
+	status                                            int
+	contents_length                                   int64
+	method, path, sourceIP, query, user_agent, errors string
+	elapsed                                           time.Duration
+}
 
 func LoggingMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-
+	start := time.Now()
+	return func(c *gin.Context) {
+		r := &requestInfo{
+			status:          c.Writer.Status(),
+			contents_length: c.Request.ContentLength,
+			path:            c.Request.URL.Path,
+			sourceIP:        c.ClientIP(),
+			query:           c.Request.URL.RawQuery,
+			user_agent:      c.Request.UserAgent(),
+			errors:          c.Errors.ByType(gin.ErrorTypePrivate).String(),
+			elapsed:         time.Since(start),
+		}
+		slog.InfoContext(c.Request.Context(), "Request Info", "Request", r.logValue())
 	}
+}
+
+func (r *requestInfo) logValue() slog.Value {
+	return slog.GroupValue(
+		slog.Int("status", r.status),
+		slog.Int64("Content-length", r.contents_length),
+		slog.String("method", r.method),
+		slog.String("path", r.path),
+		slog.String("sourceIP", r.sourceIP),
+		slog.String("query", r.query),
+		slog.String("user_agent", r.user_agent),
+		slog.String("errors", r.errors),
+		slog.Duration("elapsed", r.elapsed),
+	)
 }

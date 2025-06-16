447f180383bac69b09b618d3f92a4b35a7338e59
Nahaq789 <naha022677@icloud.com>
2025-06-16 19:03:37 +0900

    fix: router

--- services/user_service/cmd/api/router.go
+++ services/user_service/cmd/api/router.go
@@ -10,6 +10,7 @@ import (
 func Router(r gin.IRouter) error {
 	v1 := r.Group("/api/v1")
 	v1.Use(middleware.TraceMiddleware())
+	v1.Use(middleware.LoggingMiddleware())
 	v1.GET("/ping", func(c *gin.Context) {
 		slog.InfoContext(c.Request.Context(), "良いこのみんな～")
 		c.JSON(200, gin.H{

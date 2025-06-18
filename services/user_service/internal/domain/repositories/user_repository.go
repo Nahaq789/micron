e44b363e790acf6b52587b0f2d6639e3b0912b60
Nahaq789 <naha022677@icloud.com>
2025-06-18 14:49:42 +0900

    fix: user repository

--- services/user_service/internal/domain/repositories/user_repository.go
+++ services/user_service/internal/domain/repositories/user_repository.go
@@ -3,7 +3,7 @@ package repositories
 import aggregate "user_service/internal/domain/aggregates"
 
 type UserRepository interface {
-	GetById(userId int) (aggregate.User, error)
-	Register(user aggregate.User) error
-	Update(user aggregate.User) error
+	GetById(userId int) (*aggregate.User, error)
+	Register(user *aggregate.User) error
+	Update(user *aggregate.User) error
 }

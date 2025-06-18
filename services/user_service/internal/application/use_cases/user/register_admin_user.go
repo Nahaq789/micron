66e4d03b0c317dc8a2b44e0b220aabe2276fa730
Nahaq789 <naha022677@icloud.com>
2025-06-18 14:49:58 +0900

    register admin user

--- services/user_service/internal/application/use_cases/user/register_admin_user.go
+++ services/user_service/internal/application/use_cases/user/register_admin_user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log/slog"
 	"user_service/internal/application/commands"
+	aggregate "user_service/internal/domain/aggregates"
 	"user_service/internal/domain/models/user"
 	userprofile "user_service/internal/domain/models/user_profile"
 	"user_service/internal/domain/repositories"
@@ -22,14 +23,28 @@ func (r RegisterAdminUser) RegisterAdmin(ctx context.Context, c commands.Registe
 	email, err := user.NewEmail(c.GetEmail())
 	if err != nil {
 		r.logger.ErrorContext(ctx, "メールアドレスが不正です。", "error", err)
+		return err
 	}
 
 	userName, err := userprofile.NewUserName(c.GetUserName())
 	if err != nil {
 		r.logger.ErrorContext(ctx, "ユーザ名が不正です。", "error", err)
+		return err
 	}
 
 	bio := userprofile.NewBio(c.GetBio())
+
+	user, err := aggregate.RegisterAdminUser(email, userName, bio)
+	if err != nil {
+		r.logger.ErrorContext(ctx, "管理者ユーザの作成に失敗しました。", "error", err)
+		return err
+	}
+
+	registerErr := r.repository.Register(user)
+	if registerErr != nil {
+		r.logger.ErrorContext(ctx, "管理者ユーザの登録に失敗しました。", "error", err)
+		return err
+	}
 
 	return nil
 }

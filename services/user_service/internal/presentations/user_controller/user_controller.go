53124409ffdc1961668b5f3f6b899afc78204880
Nahaq789 <naha022677@icloud.com>
2025-07-09 08:14:47 +0900

    fix log

--- services/user_service/internal/presentations/user_controller/user_controller.go
+++ services/user_service/internal/presentations/user_controller/user_controller.go
@@ -2,23 +2,27 @@ package usercontroller
 
 import (
 	"context"
+	"log/slog"
 	"user_service/internal/application/commands"
 	"user_service/internal/application/use_cases/user"
 	"user_service/proto"
 )
 
 type UserController struct {
+	logger   slog.Logger
 	register user.RegisterAdminUser
 	proto.UnimplementedUserServiceServer
 }
 
-func NewUserController(register user.RegisterAdminUser) *UserController {
+func NewUserController(l slog.Logger, register user.RegisterAdminUser) *UserController {
 	return &UserController{
+		logger:   l,
 		register: register,
 	}
 }
 
 func (uc *UserController) CreateAdminUser(ctx context.Context, req *proto.CreateAdminUserRequest) (*proto.CreateAdminUserResponse, error) {
+	uc.logger.InfoContext(ctx, "管理者ユーザ作成処理を開始します。")
 	command := commands.NewRegisterAdminUserCommand(req.GetEmail(), req.GetUsername(), req.GetBio(), req.GetOrganizationId())
 	if err := uc.register.RegisterAdmin(ctx, command); err != nil {
 		return nil, err
@@ -28,5 +32,6 @@ func (uc *UserController) CreateAdminUser(ctx context.Context, req *proto.Create
 		Message: "管理者ユーザの作成に成功しました。",
 	}
 
+	uc.logger.InfoContext(ctx, "管理者ユーザ作成処理を終了します。")
 	return res, nil
 }

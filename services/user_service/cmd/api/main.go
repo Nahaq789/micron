2bc6b4fdc076008ff45bf9f83805978f941de2e3
Nahaq789 <naha022677@icloud.com>
2025-07-09 08:39:02 +0900

    grpc server

--- services/user_service/cmd/api/main.go
+++ services/user_service/cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"log"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
 	"user_service/internal/shared/infrastructure/logger"
+	"user_service/proto"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -16,6 +20,19 @@ func main() {
 	contextHandler := logger.NewContextHandler(handler)
 	logger := slog.New(contextHandler)
 	slog.SetDefault(logger)
+
+	go func() {
+		listen, err := net.Listen("tcp", "localhost:50051")
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		s := grpc.NewServer()
+		proto.RegisterUserServiceServer(s)
+		logger.Info("Server is running on :50051")
+		if err := s.Serve(listen); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
 
 	r := gin.Default()
 	if err := Router(r); err != nil {

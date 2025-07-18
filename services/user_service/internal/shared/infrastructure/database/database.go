212523b9187352f9b3c1512d4327f4b3363481d3
Nahaq789 <naha022677@icloud.com>
2025-07-18 15:12:47 +0900

    fix: database

--- services/user_service/internal/shared/infrastructure/database/database.go
+++ services/user_service/internal/shared/infrastructure/database/database.go
@@ -2,18 +2,23 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func InitDB() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "hoge")
+	cfg, err := NewDBConfig()
+	if err != nil {
+		return nil, err
+	}
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", cfg.GetUser(), cfg.GetPass(), cfg.GetName()))
 	if err != nil {
 		return nil, err
 	}
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
-	return nil, nil
+	return db, nil
 }

4908d43a3d25f3f86c78a92e761069db55198cd9
Nahaq789 <naha022677@icloud.com>
2025-08-17 15:15:14 +0900

    fix

--- justfile
+++ justfile
@@ -1,2 +1,6 @@
 @run_us:
   cd ./services/user_service/ && GO_ENV=dev go run ./...
+
+@apply:
+  ls
+  cd ./scripts/k8s && bash apply-all.sh
--- scripts/k8s/apply-all.sh
+++ scripts/k8s/apply-all.sh
@@ -2,3 +2,8 @@ kubectl apply -f ./services/database/k8s/userdb/userdb-config-map.yaml
 kubectl apply -f ./services/database/k8s/userdb/userdb-v.yaml
 kubectl apply -f ./services/database/k8s/userdb/userdb-d.yaml
 kubectl apply -f ./services/database/k8s/userdb/userdb-s.yaml
+
+kubectl apply -f ../../services/database/k8s/userdb/userdb-config-map.yaml
+kubectl apply -f ../../services/database/k8s/userdb/userdb-v.yaml
+kubectl apply -f ../../services/database/k8s/userdb/userdb-d.yaml
+kubectl apply -f ../../services/database/k8s/userdb/userdb-s.yaml
